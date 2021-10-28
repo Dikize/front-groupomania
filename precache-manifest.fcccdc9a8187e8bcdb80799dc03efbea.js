3831b3c75adf1a913e06bfa99f24e74d2ff56dc1
Kabore Lucien <dikize@icloud.com>
2021-10-28 05:00:22 +0200

    deploy dist

---
+++ precache-manifest.fcccdc9a8187e8bcdb80799dc03efbea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72e7ef11b6c816e5f5e95c6a38e1137",
+    "url": "/index.html"
+  },
+  {
+    "revision": "451f112a3b60fbfa0248",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "a2976c23c31f49c76008",
+    "url": "/static/css/main.6f453010.chunk.css"
+  },
+  {
+    "revision": "451f112a3b60fbfa0248",
+    "url": "/static/js/2.6ea7558b.chunk.js"
+  },
+  {
+    "revision": "068f7fce1dc55ec4342e218ebae8f301",
+    "url": "/static/js/2.6ea7558b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2976c23c31f49c76008",
+    "url": "/static/js/main.8acd658f.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "16bd95a8bd40439b9d6b0878a889c8d7",
+    "url": "/static/media/icon-left-font-monochrome-white.16bd95a8.svg"
+  },
+  {
+    "revision": "29a7fea093846412c4e2b803edccd7d7",
+    "url": "/static/media/wallper.29a7fea0.jpg"
+  }
+]);
